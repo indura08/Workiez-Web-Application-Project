c9d5eacda51e65d0e3887c43ba99a416a1f31a3b
indura08 <induraperera3@gmail.com>
2024-06-14 03:32:02 +0530

    fixing uncommited files from the previous commit

---
+++ frontend/workiez/src/app/components/login-user-component/login-user-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-user-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-user-component.component.html',
+  styleUrl: './login-user-component.component.css'
+})
+export class LoginUserComponentComponent {
+
+}
