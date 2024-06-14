2694d985164d52e2269bed90973f7e2eee87193c
indura08 <induraperera3@gmail.com>
2024-06-14 23:15:22 +0530

    new commits

---
+++ frontend/workiez/src/app/components/login-worker/login-worker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-worker',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-worker.component.html',
+  styleUrl: './login-worker.component.css'
+})
+export class LoginWorkerComponent {
+
+}
