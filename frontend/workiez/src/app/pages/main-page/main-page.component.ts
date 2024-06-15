a3b218bf8c83e845efe8df71f7c75dc275175e40
indura08 <induraperera3@gmail.com>
2024-06-15 13:00:33 +0530

    commiting minor changes and new component

---
+++ frontend/workiez/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+  constructor(private router: Router){}
+
+  routeFunction1(){
+    this.router.navigate(['login'])
+  }
+
+}
