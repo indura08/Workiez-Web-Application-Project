186aa640996097940d84027ff709a88c6a2ae0ee
indura08 <induraperera3@gmail.com>
2024-06-15 12:57:47 +0530

    commiting new changes

--- frontend/workiez/src/app/app.routes.ts
+++ frontend/workiez/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginUserComponentComponent } from './components/login-user-component/login-user-component.component';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
 import { LoginWorkerComponent } from './components/login-worker/login-worker.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 export const routes: Routes = [
+    {'path': "" , component: MainPageComponent},
     {'path': "login", component:LoginUserComponentComponent},
     {'path': "user/register" , component: UserRegisterComponent},
     {'path': "worker/register", component: LoginWorkerComponent},
