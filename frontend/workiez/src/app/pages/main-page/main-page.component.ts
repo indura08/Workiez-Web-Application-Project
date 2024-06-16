dd024a5a259cdb059c171ee5f55f42207898ea34
indura08 <induraperera3@gmail.com>
2024-06-17 01:33:13 +0530

    modification commits and new component commit

--- frontend/workiez/src/app/pages/main-page/main-page.component.ts
+++ frontend/workiez/src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NewsletterComponent } from '../newsletter/newsletter.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NewsletterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
