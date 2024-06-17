e48d4bb4e9d3d6e66af9e7b1e40da563e2c4f0bc
indura08 <induraperera3@gmail.com>
2024-06-17 23:52:15 +0530

    commiting new work

--- frontend/workiez/src/app/pages/main-page/main-page.component.ts
+++ frontend/workiez/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NewsletterComponent } from '../newsletter/newsletter.component';
+import { NewsletterComponent } from '../../components/newsletter/newsletter.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NewsletterComponent],
+  imports: [NewsletterComponent, FooterComponent, HeaderComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
