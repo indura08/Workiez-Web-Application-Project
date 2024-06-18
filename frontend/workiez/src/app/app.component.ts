5c3817805d66c63820b82799b127f05474effe63
indura08 <induraperera3@gmail.com>
2024-06-18 22:49:06 +0530

    commiting modifications

--- frontend/workiez/src/app/app.component.ts
+++ frontend/workiez/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { LoginUserComponentComponent } from './components/login-user-component/login-user-component.component';
+import { LoginUserComponentComponent } from './pages/login-user-component/login-user-component.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
--- frontend/workiez/src/app/app.routes.ts
+++ frontend/workiez/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginUserComponentComponent } from './components/login-user-component/login-user-component.component';
+import { LoginUserComponentComponent } from './pages/login-user-component/login-user-component.component';
 import { UserRegisterComponent } from './components/user-register/user-register.component';
-import { LoginWorkerComponent } from './components/login-worker/login-worker.component';
+import { LoginWorkerComponent } from './pages/login-worker/login-worker.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { WorkerProfileComponent } from './pages/worker-profile/worker-profile.component';
+import { JobPageComponent } from './pages/job-page/job-page.component';
 
 export const routes: Routes = [
     {'path': "" , component: MainPageComponent},
@@ -11,4 +13,7 @@ export const routes: Routes = [
     {'path': "user/register" , component: UserRegisterComponent},
     {'path': "worker/register", component: LoginWorkerComponent},
     {'path': "profile/user", component: UserProfileComponent},
+    {'path': "profile/worker", component: WorkerProfileComponent},
+    {'path': "jobs", component:JobPageComponent},
+    
 ];
