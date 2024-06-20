68d5b889207e15514696d224be62b5e6c321fc9b
indura08 <induraperera3@gmail.com>
2024-06-21 01:37:36 +0530

    commiting changes for job service because i had a email from git hub that a bearer token was exposed , i know it doesnt do anything from this deo project but keeping the practice to have some security

--- frontend/workiez/src/app/services/job.service.ts
+++ frontend/workiez/src/app/services/job.service.ts
@@ -13,7 +13,7 @@ export class JobService {
   constructor(private http: HttpClient){}
 
   public getJobs():Observable<Job[]>{
-    const headers = new HttpHeaders({"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpbmR1cmFwZXJlcmEzQGdtYWlsLmNvbSIsImlhdCI6MTcxODkxMjc2OSwiZXhwIjoxNzE4OTE0MjA5fQ.6r6uPH-diAX8aPCATd1v0nmM8QnXWqZaHGDvup2o-KM"})
+    const headers = new HttpHeaders({"Authorization": ""})
     return this.http.get<Job[]>(`${this.apiUrl}/job/all` , {headers:headers})
   }
 
