84afe589bade03b92288ef6b965ef0664d9b9005
indura08 <induraperera3@gmail.com>
2024-10-08 23:48:58 +0530

    commiting new files related tokubernetes deployment

--- frontend/workiez/src/app/components/header/header.component.ts
+++ frontend/workiez/src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { LoginService } from '../../services/login.service';
 import { User } from '../../../models/user';
 import { Worker } from '../../../models/worker';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- frontend/workiez/src/app/pages/main-page/main-page.component.ts
+++ frontend/workiez/src/app/pages/main-page/main-page.component.ts
@@ -3,11 +3,13 @@ import { Router } from '@angular/router';
 import { NewsletterComponent } from '../../components/newsletter/newsletter.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NewsletterComponent, FooterComponent, HeaderComponent],
+  imports: [NewsletterComponent, FooterComponent, HeaderComponent, RouterModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
