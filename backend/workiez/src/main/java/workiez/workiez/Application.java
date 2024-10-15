b5d3716960ebe5db9390473bc4c5c53fd8ef9d8e
indura08 <induraperera3@gmail.com>
2024-10-15 22:42:11 +0530

    commiting changes ad modification related to kubenetes deployment and service configurations

--- backend/workiez/src/main/java/workiez/workiez/Application.java
+++ backend/workiez/src/main/java/workiez/workiez/Application.java
@@ -18,7 +18,7 @@ public class Application {
 	}
 
 	@Bean
-	public CorsFilter corsFilter(){
+	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
@@ -31,23 +31,20 @@ public class Application {
 				"Origin, Accept",
 				"X-Requested-With",
 				"Access-Control-Request-Method",
-				"Access-Control-Request-Headers"
-		));
+				"Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList(
 				"Origin",
 				"Content-Type",
 				"Accept",
 				"Authorization",
 				"Access-Control-Allow-Origin",
-				"Access-Control-Allow-Credentials"
-		));
+				"Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList(
 				"GET",
 				"POST",
 				"PUT",
 				"DELETE",
-				"OPTIONS"
-		));
+				"OPTIONS"));
 
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
--- frontend/workiez/src/environment/environment.ts
+++ frontend/workiez/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:false,
-    apiBaseUrl: 'http://spring-backend.default.svc.cluster.local:8080/api',
+    apiBaseUrl: 'http://localhost:8080/api',
     userToken: 'Bearer ',
     workerToken: 'Bearer '
 }
